41fdf4a23ca0364be54101393d5b632cb738cf52
mdrodri <luis.rodriguez5@walmart.com>
2016-04-11 14:52:13 -0500

    Update in server IP

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.get('/sessions/:id', sessions.findById);
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
-app.listen(app.get('port'), function () {
+//app.listen(app.get('port'), function () {
+//    console.log('Express server listening on port ' + app.get('port'));
+//});
+
+app.listen(app.get('port'), app.get('ip'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
